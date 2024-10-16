34bddad1dd5c20c849b53f986a9885782eab76d3
Takaiii <nipas.devs@gmail.com>
2024-10-16 17:23:26 +0700

    Done problem test

---
+++ problem.js
@@ -0,0 +1,33 @@
+const numMapObject = {
+    A: 1,
+    B: 5,
+    Z: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    R: 1000
+}
+
+const convertToNumber = (str) => {
+    let splited_nums = str.split('').map(char => numMapObject[char]);
+
+    return splited_nums.reduce((total, current, index) => {
+        const prev = splited_nums[index - 1]
+        const next = splited_nums[index + 1]
+        console.log(prev)
+        console.log(current)
+        console.log(next)
+        console.log("-------------------")
+        if (next && current < next) {
+          return total + (next - current)
+        } else if (index > 0 && prev < current) {
+          return total
+        } else {
+          return total + current
+        }
+    }, 0)
+}
+
+console.log(convertToNumber("AAA"))
+console.log(convertToNumber("RCRZCAB"))
+console.log(convertToNumber("LBAAA"))
